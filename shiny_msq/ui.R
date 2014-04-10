323c53fd8f910d776aa96229ac743275be39391f
nachalca <ialvarez@iastate.edu>
2014-04-09 22:29:57 -0500

    order brackets in UI.r

--- shiny_msq/ui.R
+++ shiny_msq/ui.R
@@ -1,3 +1,7 @@
+
+library(shiny)
+library(reshape2)
+
 msq<- read.csv('msqdata.csv', header=T)
 msq.ind.aux <- melt(msq[,c(1,2,40:46)],id.vars=c('site', 'year'))
 colnames(msq.long.aux)[3:4] <- c('Index', 'Index.val')
@@ -5,67 +9,55 @@ lab.index<-c("Abundance","SpeciesRichness", "DominanceBP","Simpson",  "Shannon" 
 msq.long <- read.csv('msq_long.csv', header=T)
 msq.res<-msq[,c('site','year','Abundance','SpeciesRichness','DominanceBP', 'Simpson', 'Shannon', 'Evenness', 'AevexansRatio')]
 
-
-library(shiny)
 cap1<-'Yearly species proportion per site. The red line represents the mean proportion for each species across sites'
 
 shinyUI(fluidPage(
   titlePanel("Shine Mosquito shine!"), 
-  
-  
-  fluidRow(
-  sidebarLayout(
-  
-  sidebarPanel(width=3,
-              
-    conditionalPanel(condition="input.conditionedPanels==1",
-                     
-                    img(src="Mosquito_da_Dengue_by_Lukemaciel.png", height = 100, width = 300), 
+    fluidRow(
+      sidebarLayout(
+        sidebarPanel(width=3,
+          conditionalPanel(condition="input.conditionedPanels==1",
+                     img(src="Mosquito_da_Dengue_by_Lukemaciel.png", height = 100, width = 300), 
                      #checkboxGroupInput(inputId = "specie",
-     #                   label = "Select specie:",
-      #                 choices = levels(msq.long$specie),selected='Aedes.albopictus'),
-    selectInput(inputId = "specie",
-                        label = "Select specie:",
-                      choices = levels(msq.long$specie),selected='Aedes.vexans'),
+                     #     label = "Select specie:",
+                     #    choices = levels(msq.long$specie),selected='Aedes.albopictus'),
+                    selectInput(inputId = "specie",
+                            label = "Select specie:",
+                            choices = levels(msq.long$specie),selected='Aedes.vexans'),
      
-    checkboxGroupInput(inputId = "site",
-                       label = "Select site:",
-                       choices = levels(msq.long$site),selected='Cfall')
-    ),
+                    checkboxGroupInput(inputId = "site",
+                            label = "Select site:",
+                            choices = levels(msq.long$site),selected='Cfall')
+                          ),
     
-    conditionalPanel(condition="input.conditionedPanels==2",
+          conditionalPanel(condition="input.conditionedPanels==2",
                      checkboxGroupInput(inputId = "specie2",
                                  label = "Select specie:",
                                  choices = levels(msq.long$specie),selected='Aedes.vexans')
-                     ),
+                          ),
     
-    
-    conditionalPanel(condition="input.conditionedPanels==3",
-                     
+          conditionalPanel(condition="input.conditionedPanels==3",
                      checkboxGroupInput(inputId = "index",
                                         label = "Select index:",
                                         choices = lab.index,selected='Simpson'),
-  
                      checkboxGroupInput(inputId = "site3",
                                         label = "Select site:",
                                         choices = levels(msq.long$site),selected='Cfall')
-    
-    )
+                          )
 
-  ),
+                  ), # close sidebarPanel
   
-  mainPanel(
-    tabsetPanel(
-          tabPanel("Average",p(cap1), plotOutput("plot1"),value=1),    
-    tabPanel("Location", plotOutput("plot2"),value=2), 
-    tabPanel("Simpson", plotOutput("plot3"),value=3),
-    id="conditionedPanels" )
-       
+    mainPanel(
+        tabsetPanel(
+            tabPanel("Average",p(cap1), plotOutput("plot1"),value=1),    
+            tabPanel("Location", plotOutput("plot2"),value=2), 
+            tabPanel("Simpson", plotOutput("plot3"),value=3),
+            id="conditionedPanels" )
+            ) # close mainPanel
     
-    )
-  )
-    )
- 
-))
+          ) # close sidebarLayout
+        ) # close fluidRow
+      ) # close fluidPage 
+    ) # close shinyUI
 
 
